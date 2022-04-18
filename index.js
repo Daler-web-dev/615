d575eef32d58d46ec20c1450c7549c454bac16a5
daler-web-dev <dsarifkulo021@gmail.com>
2022-04-18 20:47:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// string
+
+// let str = 'lorem ipsum'
+
+// str.toUpperCase()
+// str.toLowerCase()
+// str.slice(1,3)
+// str.search('sum')
+// str.length
+// str.trim()
+// str.split(' ')
+// str.charAt(4)
+// str[4]
